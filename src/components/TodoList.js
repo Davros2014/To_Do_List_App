4cf64d970751b15b9ead6e4e78ee8498e318c1f9
David Parsons <info@dpdesigns.eu>
2020-05-07 14:52:55 +0200

    version prior to using context

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -38,29 +38,3 @@ function TodoList({ todos, removeToDo, toggleTodo, editTodo }) {
   return null;
 }
 export default TodoList;
-
-// function TodoList({ todos, removeTodo, toggleTodo, editTodo }) {
-//   if (todos.length)
-//     return (
-//       <Paper>
-//         <List>
-//           {todos.map((todo, i) => (
-//             // To add a key to a fragment, we have to use the long-hand version
-//             // rather than <> </>, we have to use <React.Fragment>
-//             <React.Fragment key={i}>
-//               <ToDo
-//                 {...todo}
-//                 key={todo.id}
-//                 removeTodo={removeTodo}
-//                 toggleTodo={toggleTodo}
-//                 editTodo={editTodo}
-//               />
-//               {i < todos.length - 1 && <Divider />}
-//             </React.Fragment>
-//           ))}
-//         </List>
-//       </Paper>
-//     );
-//   return null;
-// }
-// export default TodoList;
